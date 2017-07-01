d28ed3c34e86d8677f9d8732b1cfa452871f5fb9
qamarali <qamarali@qamaralis-MacBook-Pro.local>
2017-07-01 17:24:17 -0500

    Added firebase framework.

--- snapchatApp/ViewController.swift
+++ snapchatApp/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+
+    @IBAction func turnUpTapped(_ sender: Any) {
+        
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: {
+            (user,error) in
+            
+            print(" under Finally")
+            
+            if (error != nil ) {
+            print("Hey we have an error")
+            }
+                else
+                {
+                    print("Signed In")
+            }
+            
+        })
+    }
 
 }
 
