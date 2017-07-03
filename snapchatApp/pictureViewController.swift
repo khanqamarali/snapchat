88bce74091298dfb3658e2666fd278ffa4ac7cc4
qamarali <qamarali@qamaralis-MacBook-Pro.local>
2017-07-02 23:02:22 -0500

    Added imagpicker follow.

--- snapchatApp/pictureViewController.swift
+++ snapchatApp/pictureViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class pictureViewController: UIViewController {
+class pictureViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var descText: UITextField!
     @IBOutlet weak var displayImage: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    imagePicker.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        displayImage.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
     @IBAction func cameraButtonTapped(_ sender: Any) {
-         print("I am camera")
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false  ;
+        present(imagePicker,animated: true,completion: nil)
     }
     
     @IBAction func nextTapped(_ sender: Any) {
