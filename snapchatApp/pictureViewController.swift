1c117fead278821cf13aa56af8c6f5aa11909ede
qamarali <qamarali@qamaralis-MacBook-Pro.local>
2017-07-03 22:33:52 -0500

    Fixed image picker issue

--- snapchatApp/pictureViewController.swift
+++ snapchatApp/pictureViewController.swift
@@ -13,27 +13,31 @@ class pictureViewController: UIViewController,UIImagePickerControllerDelegate,UI
     @IBOutlet weak var descText: UITextField!
     @IBOutlet weak var displayImage: UIImageView!
     
-    var imagePicker = UIImagePickerController()
+ var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    imagePicker.delegate = self
+            imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
-        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         
         displayImage.image = image
         
-        imagePicker.dismiss(animated: true, completion: nil)
+         imagePicker.dismiss(animated: true, completion: nil)
+        
     }
-
+    
+    
+    
     @IBAction func cameraButtonTapped(_ sender: Any) {
-        imagePicker.sourceType = .savedPhotosAlbum
-        imagePicker.allowsEditing = false  ;
-        present(imagePicker,animated: true,completion: nil)
+      print("camera tapped")
+                imagePicker.sourceType = .photoLibrary
+        //        imagePicker.allowsEditing = false  ;
+                present(imagePicker,animated: true,completion: nil)
     }
     
     @IBAction func nextTapped(_ sender: Any) {
